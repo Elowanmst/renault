9eec97b0ca5dc3201138f62601af23db0cdf1af9
Elowan MESTRES <elowanmestres@gmail.com>
2025-03-12 15:17:19 +0100

    structure de base initialiser

---
+++ accueil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Garage Renault - Dacia Rond-Point de l'Europe">
+    <meta name="keywords" content="garage, réparations, vente de véhicule, véhicule, dacia, renault">
+    <meta name="author" content="EC-Craftt">
+    <title>Garage Renault</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
+    </style>
+</head>
+
+<body>  
+
+
+</body>
+
